fb327a6360563c9a0dba33f4256790f3986e51b9
CzE <cze@clashmc.co>
2020-05-27 21:22:32 -0400

    Created RTPInfoRequestDisconnection

---
+++ comms/packets/info/request/RTPInfoRequestDisconnection.java
@@ -0,0 +1,9 @@
+package com.rtbytez.common.comms.packets.info.request;
+
+import com.rtbytez.common.comms.packets.RTPacketRequest;
+
+public class RTPInfoRequestDisconnection extends RTPacketRequest {
+    public RTPInfoRequestDisconnection(String header) {
+        super(header);
+    }
+}
